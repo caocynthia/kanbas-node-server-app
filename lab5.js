1c34a3a7df6ba87acc5ef5bd14f040bc487d739f
caocynthia <cynthiacao.work@gmail.com>
2023-11-23 13:56:42 -0500

    update

--- lab5.js
+++ lab5.js
@@ -33,10 +33,6 @@ function Lab5(app) {
     res.json(assignment.title);
   });
 
-  app.get("/a5/assignment/description", (req, res) => {
-    res.json(assignment.description);
-  });
-
   app.get("/a5/calculator", (req, res) => {
     const { a, b, operation } = req.query;
     let result = 0;
@@ -89,19 +85,31 @@ function Lab5(app) {
     res.json(newTodo);
   });
 
-  // app.get("/a5/todos/create", (req, res) => {
-  //   const newTodo = {
-  //     id: new Date().getTime(),
-  //     title: "New Task",
-  //     completed: false,
-  //   };
-  //   todos.push(newTodo);
-  //   res.json(todos);
-  // });
+  app.put("/a5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    if (!todo) {
+      res.res
+        .status(404)
+        .json({ message: `Unable to update Todo with ID ${id}` });
+      return;
+    }
+    todo.title = req.body.title;
+    todo.description = req.body.description;
+    todo.due = req.body.due;
+    todo.completed = req.body.completed;
+    res.sendStatus(200);
+  });
 
   app.delete("/a5/todos/:id", (req, res) => {
     const { id } = req.params;
     const todo = todos.find((t) => t.id === parseInt(id));
+    if (!todo) {
+      res.res
+        .status(404)
+        .json({ message: `Unable to delete Todo with ID ${id}` });
+      return;
+    }
     todos.splice(todos.indexOf(todo), 1);
     res.sendStatus(200);
   });
